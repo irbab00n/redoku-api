d05a139eb260e235bdb2e0debd0781b97d2b9077
Thomas Cosby <cosbyts@gmail.com>
2019-03-03 13:57:28 -0800

    Add comments to Solution controller methods

--- controllers/Solution.js
+++ controllers/Solution.js
@@ -3,10 +3,21 @@ const Joi = require('joi');
 const { Solution } = require('../models');
 const schemas = require('../schemas');
 
+/**
+ * Returns a promise initiated from the supplied puzzle_ids
+ * @param {Number} puzzleId - ID of the puzzle we want to pull out all of the solutions for
+ * @returns {Promise} - Knex-based promise returned with 'then' and 'catch' blocks available
+ */
 module.exports.findSolutions = (puzzleId) => {
   return Solution.where({puzzle_id: puzzleId}).fetchAll();
 }
 
+/**
+ * Checks for a supplied solution in the request body, validates the shape using JOI
+ * Creates a new solution entry and returns that solution to the user
+ * @param {Object} req - Request object from Express
+ * @param {Object} res - Response object from Express
+ */
 module.exports.create = (req, res) => {
   const { solution } = req.body;
 
@@ -22,23 +33,17 @@ module.exports.create = (req, res) => {
     console.log(`\n${error.name}:\n\n`, error.details);
     res.status(422).send(error.details);
     return;
-  } else {
-    Solution.forge(solution).save()
-      .then(solution => {
-        res.status(201).send(solution);
-      })
-      .catch(error => {
-        console.log('Something went wrong while attempting to save a new solution: ', error);
-        res.status(500).send({
-          message: 'Something went wrong while attempting to save a new solution',
-          error
-        });
+  } 
+
+  Solution.forge(solution).save()
+    .then(solution => {
+      res.status(201).send(solution);
+    })
+    .catch(error => {
+      console.log('Something went wrong while attempting to save a new solution: ', error);
+      res.status(500).send({
+        message: 'Something went wrong while attempting to save a new solution',
+        error
       });
-  }
+    });
 };
-
-// module.exports.delete = (req, res) => {
-
-//   res.status(202).send('Successfully removed a Solution');
-
-// };
